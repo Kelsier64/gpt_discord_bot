1c1582e54495c9fc1e085b4e7c18784d975320e4
Kelsier <evan4556376@yahoo.com.tw>
2024-10-06 02:52:51 +0800

    history

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -6,8 +6,11 @@ import requests
 import base64
 class Gpt(commands.Cog):
     gpt_state = True
+    history = []
+
     def __init__(self,bot):
         self.bot = bot
+
     @nextcord.slash_command(name="activate",description="activate the gpt",guild_ids=[1270418744434491413,1156116900321960017])
     async def activate(self,interaction:Interaction):
         self.gpt_state = True
@@ -17,7 +20,21 @@ class Gpt(commands.Cog):
     async def deactivate(self,interaction:Interaction):
         self.gpt_state = False
         await interaction.response.send_message("gpt is now deactivated")
+    
 
+    @nextcord.slash_command(name="read",description="read message history",guild_ids=[1270418744434491413,1156116900321960017])
+    async def read(self,interaction:Interaction,limit:int):
+        channel = interaction.channel
+        self.history.clear()
+        async for msg in channel.history(limit=limit):
+            self.history.append(f"{msg.author}: {msg.content}")
+        await interaction.response.send_message(f"read {limit} messages done")
+        
+    @nextcord.slash_command(name="clear",description="clear message history",guild_ids=[1270418744434491413,1156116900321960017])
+    async def deactivate(self,interaction:Interaction):
+        self.history.clear()
+        await interaction.response.send_message("cleared messages done")
+    
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if message.author == self.bot.user or not self.gpt_state:
@@ -29,11 +46,17 @@ class Gpt(commands.Cog):
                     img = base64.b64encode(response.content).decode('utf-8')
 
                     msgs = [{"role": "user","content": [{"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{img}"}}]},
-                            {"role": "user", "content": message.content}]
+                            {"role": "user", "content": f"{message.author}: {message.content}"}]
+                    
             else:
-                msgs = [{"role": "user", "content": message.content}]
+                msgs = [{"role": "system", "content": f"以下是對話歷史記錄:{self.history} 請根據歷史記錄回答user"},{"role": "user", "content": f"{message.author}: {message.content}"}]
+                self.history.append(f"{message.author}: {message.content}")
+
             reply = openai_api.str_request(msgs, 500)
+            self.history.append(f"me: {reply}")
+
             await message.channel.send(reply)
+            
 
 def setup(bot):
     bot.add_cog(Gpt(bot))
--- test0.py
+++ test0.py
@@ -2,5 +2,5 @@ import openai_api
 
 
 msg = [{"role": "user", "content": "hi"}]
-reply = openai_api.str_request(msg,500)
-print(reply)
+
+print(f"haha{msg}")
