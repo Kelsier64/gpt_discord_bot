3b8c4dda79c8412129f5c002ff3727dd0d8a337d
Kelsier <evan4556376@yahoo.com.tw>
2024-10-06 03:57:24 +0800

    guild and img content bug

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -11,18 +11,17 @@ class Gpt(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="activate",description="activate the gpt",guild_ids=[1270418744434491413,1156116900321960017])
+    @nextcord.slash_command(name="activate",description="activate the gpt")
     async def activate(self,interaction:Interaction):
         self.gpt_state = True
         await interaction.response.send_message("gpt is now activated")
 
-    @nextcord.slash_command(name="deactivate",description="deactivate the gpt",guild_ids=[1270418744434491413,1156116900321960017])
+    @nextcord.slash_command(name="deactivate",description="deactivate the gpt")
     async def deactivate(self,interaction:Interaction):
         self.gpt_state = False
         await interaction.response.send_message("gpt is now deactivated")
     
-
-    @nextcord.slash_command(name="read",description="read message history",guild_ids=[1270418744434491413,1156116900321960017])
+    @nextcord.slash_command(name="read",description="read message history")
     async def read(self,interaction:Interaction,limit:int):
         channel = interaction.channel
         self.history.clear()
@@ -30,8 +29,8 @@ class Gpt(commands.Cog):
             self.history.append(f"{msg.author}: {msg.content}")
         await interaction.response.send_message(f"read {limit} messages done")
         
-    @nextcord.slash_command(name="clear",description="clear message history",guild_ids=[1270418744434491413,1156116900321960017])
-    async def deactivate(self,interaction:Interaction):
+    @nextcord.slash_command(name="clear",description="clear message history")
+    async def clear(self,interaction:Interaction):
         self.history.clear()
         await interaction.response.send_message("cleared messages done")
     
@@ -47,7 +46,7 @@ class Gpt(commands.Cog):
 
                     msgs = [{"role": "user","content": [{"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{img}"}}]},
                             {"role": "user", "content": f"{message.author}: {message.content}"}]
-                    
+                    self.history.append(f"{message.author}: {message.content}")
             else:
                 msgs = [{"role": "system", "content": f"以下是對話歷史記錄:{self.history} 請根據歷史記錄回答user"},{"role": "user", "content": f"{message.author}: {message.content}"}]
                 self.history.append(f"{message.author}: {message.content}")
